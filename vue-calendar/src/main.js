c82fe652c14cc8ccdf08f12fe4f0af03bc573470
Ian Kachan <kachan.yanf@gmail.com>
2020-04-26 05:28:46 +0500

    Added the calendar template from the documentation

--- vue-calendar/src/main.js
+++ vue-calendar/src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
+Vue.use(VueTextareaAutosize);
 
 Vue.config.productionTip = false
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAeRm818KEQ8TTb1ezjFot3Joi9iUCAEOw",
+  authDomain: "calendar-vuejs-ff247.firebaseapp.com",
+  databaseURL: "https://calendar-vuejs-ff247.firebaseio.com",
+  projectId: "calendar-vuejs-ff247",
+  storageBucket: "calendar-vuejs-ff247.appspot.com",
+  messagingSenderId: "595208383763",
+  appId: "1:595208383763:web:6de9c422d6be4144d87ee6"
+});
+
+export const db = firebase.firestore;
 
 new Vue({
   vuetify,
